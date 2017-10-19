969451746fe6bbc3a3b7488d35ad3bdc7c7aae4c
kastelecn <nika.kastelec@gmail.com>
2017-10-19 20:46:28 +0200

    speremembe

---
+++ ChitChat/src/ChitChat.java
@@ -0,0 +1,12 @@
+public class ChitChat {
+
+	public static void main(String[] args) {
+		ChatFrame chatFrame = new ChatFrame();
+		DrugiRobot robotek = new DrugiRobot(chatFrame);
+		robotek.activate();
+		chatFrame.pack();
+		chatFrame.setVisible(true);
+		chatFrame.setTitle("Klepetalnica");
+	}
+
+}
